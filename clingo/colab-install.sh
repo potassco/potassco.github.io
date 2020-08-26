1ee32c62926facd59c68a13922142e619994d93d
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2020-08-26 16:32:39 +0200

    rename

--- clingo/colab-install.sh
+++ clingo/colab-install.sh
@@ -1,7 +1,7 @@
-clingo_version=${CLINGO_VERSION=5.4.0}
+version=${VERSION=5.4.0}
 
-wget -qc "https://github.com/potassco/clingo/archive/v${clingo_version}.tar.gz"
-tar xf "v${clingo_version}.tar.gz"
+wget -qc "https://github.com/potassco/clingo/archive/v${version}.tar.gz"
+tar xf "v${version}.tar.gz"
 mkdir -p build
-cmake -H"clingo-${clingo_version}" -Bbuild -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYCLINGO_USER_INSTALL=off
+cmake -H"clingo-${version}" -Bbuild -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYCLINGO_USER_INSTALL=off
 cmake --build build --target install
